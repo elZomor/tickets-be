39a9205bacd08a2d913fbab5f023b4a3a7b043ef
Mohamed Elzomor <mohamed.elzomor@outlook.com>
2024-09-14 02:10:55 +0300

    update clerk serializer

--- clerk/serializer.py
+++ clerk/serializer.py
@@ -5,18 +5,23 @@ from rest_framework import serializers
 from rest_framework.exceptions import APIException
 
 from config.constants import CLERK_SIGNING_SECRET
+from svix import Webhook
+from svix.webhooks import WebhookVerificationError
 
 
 class ClerkSerializer(serializers.Serializer):
-    clerk_signature = serializers.CharField(required=True)
-    body = serializers.JSONField(required=True)
+    svix_id = serializers.CharField(required=True)
+    svix_timestamp = serializers.CharField(required=True)
+    svix_signature = serializers.CharField(required=True)
+    body = serializers.CharField(required=True)
 
     def validate(self, data):
-        expected_signature = hmac.new(
-            CLERK_SIGNING_SECRET.encode('utf-8'),
-            msg=data.get('body').encode('utf-8'),
-            digestmod=hashlib.sha256,
-        ).hexdigest()
-        signature = data.get('clerk_signature')
-        if not hmac.compare_digest(signature, expected_signature):
+        webhook = Webhook(CLERK_SIGNING_SECRET)
+        try:
+            webhook.verify(data.get('body'), {
+                'svix-id': data.get('svix_id'),
+                'svix-timestamp': data.get('svix_timestamp'),
+                'svix-signature': data.get('svix_signature'),
+            })
+        except WebhookVerificationError as e:
             raise APIException()
--- clerk/views.py
+++ clerk/views.py
@@ -1,13 +1,19 @@
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
 
+from clerk.serializer import ClerkSerializer
+
 
 class ClerkViewSet(viewsets.ViewSet, mixins.CreateModelMixin):
 
     def create(self, request, *args, **kwargs):
-        print("*" * 20, flush=True)
-        print(request.body, flush=True)
-        print(request.data, flush=True)
-        print(request.headers, flush=True)
-        print("*" * 20, flush=True)
+        serializer_data = {
+            'body': request.body,
+            'svix_id': request.headers.get('svix_id'),
+            'svix_timestamp': request.headers.get('svix_timestamp'),
+            'svix_signature': request.headers.get('svix_signature'),
+        }
+        serializer = ClerkSerializer()
+        serializer.validate(data=serializer_data)
+        print("success", flush=True)
         return Response({'status': 'success'})
