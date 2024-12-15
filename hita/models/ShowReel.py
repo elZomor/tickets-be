ff68b2f5b7aec58eaccf7a7e3bdbca4c840b2c95
zomor <mohamed.elzomor@outlook.com>
2024-12-16 01:04:19 +0200

    fix showreel migration

--- hita/models/ShowReel.py
+++ hita/models/ShowReel.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-from utils.code_utils import get_upload_path, validate_file_size
+from utils.code_utils import validate_file_size
 
 
 class ShowReel(models.Model):
     performer = models.OneToOneField(
         'hita.Performer', on_delete=models.CASCADE, related_name='show_reel'
     )
-    file = models.FileField(upload_to=get_upload_path, validators=[validate_file_size])
+    file = models.FileField(storage='storages.backends.s3boto3.S3Boto3Storage', validators=[validate_file_size])
