217ce27866310aff6d293ba0c8f1166293949f48
zomor <mohamed.elzomor@outlook.com>
2025-01-03 13:35:55 +0200

    feat(AC-28): refine the celery task view on admin tool

--- utils_app/admin.py
+++ utils_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django_celery_results.models import TaskResult
 
+admin.site.unregister(TaskResult)
 @admin.register(TaskResult)
 class TaskResultAdmin(admin.ModelAdmin):
-    list_display = ('task_id', 'status', 'date_done', 'result')
+    list_display = ('date_done', 'result', 'task_kwargs')
     list_filter = ('status', 'date_done')
     search_fields = ('task_id', 'result')
     readonly_fields = ('task_id', 'status', 'date_done', 'result', 'traceback', 'meta', 'task_name')
