2cd49e188d94a80ebc32334d4556cb2b074fa730
zomor <mohamed.elzomor@outlook.com>
2025-01-04 20:08:38 +0200

    bug(AC-29): use pre-served list instead of the queryset to avoid filters

--- hita/admin.py
+++ hita/admin.py
@@ -87,12 +87,13 @@ class HITAMemberAdmin(admin.ModelAdmin):
     @admin.action(description='Approve request')
     @prevent_update_approved_members
     def approve_request(modeladmin, request, queryset, hita_member):
+        approved_members = list(queryset)
         updated_count = queryset.update(
             request_status=Status.APPROVED.value,
             reviewed_by=hita_member,
             reviewed_at=datetime.now(),
         )
-        for member in queryset:
+        for member in approved_members:
             send_approve_email.delay(to_email=member.user.email)
         modeladmin.message_user(request, f'{updated_count} items marked as approved.')
 
