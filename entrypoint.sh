d2232a4e15feaccda7b984b0159c57c6d0cee7dd
zomor <mohamed.elzomor@outlook.com>
2025-08-08 14:56:07 +0300

    add S3 to url in Actogram

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,4 +26,8 @@ fi
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-exec python manage.py runserver 0.0.0.0:8000
+if [ "$DEBUG" = "true" ]; then
+  exec python manage.py runserver 0.0.0.0:8000
+else
+  exec gunicorn config.wsgi:application -b 0.0.0.0:8000 -w 4
+fi
